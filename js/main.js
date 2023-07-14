bfaf52d4cefee0c59dce02031631acea53153076
Athanasios Toptsios <atoptsios@allweb.gr>
2023-07-14 13:26:39 +0300

    add effects

--- js/main.js
+++ js/main.js
@@ -343,4 +343,40 @@ mailChimp();
             $("#search_input").focus();
         });
 
+
+$(document).ready(function() {
+    
+        $('.fadein').each( function(i){
+            
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1'},1300);
+            }
+            
+        }); 
+    
+});
+
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.fadeinleft').each( function(i){
+            
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1','margin-left':'0px'},400);
+            }
+            
+        }); 
+    });
+});
+
+$(document).ready(function () {
+    $('.mytitle').fadeIn(1000).removeClass('hidden');
+});
+
+
 })(jQuery);	
