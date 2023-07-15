3a8a17511c9ca24a43513f1924231ac3aab965d1
Athanasios Toptsios <atoptsios@allweb.gr>
2023-07-15 13:39:59 +0300

    fix fade in left and toggler button

--- js/main.js
+++ js/main.js
@@ -363,11 +363,15 @@ $(document).ready(function() {
     $(window).scroll( function(){
         $('.fadeinleft').each( function(i){
             
-            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            // var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            // var bottom_of_window = $(window).scrollTop() + $(window).height();
             
-            if( bottom_of_window > bottom_of_element ){
-                $(this).animate({'opacity':'1','margin-left':'0px'},400);
+			let position = $(this).offset().top,
+                  scroll = $(window).scrollTop(),
+                  windowHeight = $(window).height();
+			
+            if( scroll > position - windowHeight + 80 ){
+                $(this).animate({'opacity':'1','margin-left':'0px'},600);
             }
             
         }); 
